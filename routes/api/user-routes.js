90c1d8581c6a528e1acece614cf07db8ca62a02d
Shane Dinh <shane.dinh5@gmail.com>
2022-09-13 19:20:13 -0500

    POST/DELETE for friends

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -3,12 +3,19 @@ const {
   getAllUsers,
   getUserByID,
   createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
 } = require("../../controllers/user-controller");
 
-// set up GET all and Post at /api/users
+// set up GET all and POST at /api/users
 router.route("/").get(getAllUsers).post(createUser);
 
 // set up GET one, PUT, and DELETE at /api/users/:id
 router.route("/:id").get(getUserByID).put(updateUser).delete(deleteUser);
+
+// set up POST and DELETE at /api/:id/friends/:friendId
+router.route("/:id/friends/:friendId").post(addFriend).delete(deleteFriend);
 
 module.exports = router;
